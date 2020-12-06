bae15aecf5b6715a9b390fc4c91ff3ccb315769c
Ralph Jones <ralph.f.jones@gmail.com>
2020-12-06 11:06:39 +0100

    added einstufungstest_business_englisch.php

---
+++ einstufungstest_business_englisch.php
@@ -0,0 +1,11 @@
+<?php
+
+$page_language="DE";
+$language="English";
+$target_lang_type="Business";
+$target_lang="Englisch";
+$testdata="business_english.inc";
+
+require "placementtest.inc";
+
+?>
