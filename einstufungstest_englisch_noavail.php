d96186ab38b927757a8981ceb43455d1f4fd8d4c
Ralph Jones <ralph.f.jones@gmail.com>
2025-10-29 11:19:21 +0100

    added new page without availability questions

---
+++ einstufungstest_englisch_noavail.php
@@ -0,0 +1,11 @@
+<?php
+
+$page_language="DE";
+$language="English";
+$target_lang_type="";
+$target_lang="Englisch";
+$testdata=$language.".inc";
+
+require "placementtest_noavail.inc";
+
+?>
---
+++ placementtest_english_noavail.php
@@ -0,0 +1,11 @@
+<?php
+
+$page_language="EN";
+$language="English";
+$target_lang_type="";
+$target_lang="English";
+$testdata=$language.".inc";
+
+require "placementtest_noavail.inc";
+
+?>
